78873f082a5e4965e8aa4bfacfd87667857aa15a
Gunnar Andreas Skogvold <andreas.skogvold@conceptos.no>
2018-08-28 14:53:58 +0200

    Added led driver test for constructor

--- code/unity/MyLedDriver/MyLedDriverTest.c
+++ code/unity/MyLedDriver/MyLedDriverTest.c
@@ -1,4 +1,5 @@
 #include "unity_fixture.h"
+#include "stdint.h"
 
 TEST_GROUP(MyLedDriver);
 
@@ -14,5 +15,7 @@ TEST_TEAR_DOWN(MyLedDriver)
 
 TEST(MyLedDriver, LedsOffAfterCreate)
 {
-    TEST_FAIL_MESSAGE("Start here");
+    uint16_t virtualLeds = 0xffff;
+    MyLedDriver_Create(&virtualLeds);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
 }
