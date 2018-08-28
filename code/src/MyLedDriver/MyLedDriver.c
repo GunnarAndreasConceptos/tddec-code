58cbcef15747f67f0dd9191fd3ca677f671a0b40
Gunnar Andreas Skogvold <andreas.skogvold@conceptos.no>
2018-08-28 15:36:20 +0200

    Added implementation of turn on led function

--- code/src/MyLedDriver/MyLedDriver.c
+++ code/src/MyLedDriver/MyLedDriver.c
@@ -1,13 +1,16 @@
 #include "MyLedDriver.h"
 
+static uint16_t* ledsAddress;
+
 void MyLedDriver_Create(uint16_t *address)
 {
-    *address = 0;
+    ledsAddress = address;
+    *ledsAddress = 0;
 }
 
 void MyLedDriver_TurnOn(int ledNumber)
 {
-    
+    *ledsAddress = 1;
 }
 
 void MyLedDriver_Destroy(void)
