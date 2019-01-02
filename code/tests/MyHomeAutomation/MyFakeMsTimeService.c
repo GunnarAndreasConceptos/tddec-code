b76f4f09374e328a0b4f0ed30a1908e26e98ff91
Gunnar Andreas Skogvold <andreas.skogvold@conceptos.no>
2019-01-02 13:21:24 +0100

    Added passing set ms time logic

--- code/tests/MyHomeAutomation/MyFakeMsTimeService.c
+++ code/tests/MyHomeAutomation/MyFakeMsTimeService.c
@@ -1,9 +1,10 @@
 #include "MyFakeMsTimeService.h"
 
+static MsTime localTime;
 
 void MyFakeMsTimeService_Create(void)
 {
-
+    localTime.msec = TIME_UNKNOWN;
 }
 
 void MyFakeMsTimeService_Destroy(void)
@@ -13,10 +14,10 @@ void MyFakeMsTimeService_Destroy(void)
 
 void MyMsTimeService_GetTime(MsTime * time)
 {
-    time->msec = TIME_UNKNOWN;
+    time->msec = localTime.msec;
 }
 
 void MyFakeMsTimeService_SetMilliSeconds(long msec)
 {
-    
+    localTime.msec = msec;
 }
