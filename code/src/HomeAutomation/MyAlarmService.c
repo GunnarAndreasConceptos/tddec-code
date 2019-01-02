952f0194530444253d668867b9a6e12f7cebd37b
Gunnar Andreas Skogvold <andreas.skogvold@conceptos.no>
2019-01-02 13:36:24 +0100

    Added logic to schedule a single alarm

--- code/src/HomeAutomation/MyAlarmService.c
+++ code/src/HomeAutomation/MyAlarmService.c
@@ -1,25 +1,36 @@
 #include "MyAlarmService.h"
 #include "common.h"
 
+
+typedef struct Alarm {
+    AlarmCallback cb;
+    long msEpochTime;
+} Alarm;
+
+static Alarm alarm;
+
 void MyAlarmService_Create(void)
 {
-
+    alarm.cb = NULL;
+    alarm.msEpochTime = -1;
 }
+
 void MyAlarmService_Destroy(void)
 {
 
 }
 AlarmCallback MyAlarmService_GetLastCallback(void)
 {
-    return NULL;
+    return alarm.cb;
 }
 
 long MyAlarmService_GetLastTimestamp(void)
 {
-    return -1;
+    return alarm.msEpochTime;
 }
 
 void MyAlarmService_ScheduleAlarm(long msEpochTime, AlarmCallback cb)
 {
-
+    alarm.cb = cb;
+    alarm.msEpochTime = msEpochTime;
 }
